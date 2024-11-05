59464c10f9e65e03fcc60e3d27684a824758d4fb
Johan Svensson <johan@exclude.se>
2024-11-05 13:50:07 +0100

    register NSD service when recording

--- app/src/main/java/com/babysleep/ChildActivity.kt
+++ app/src/main/java/com/babysleep/ChildActivity.kt
@@ -1,6 +1,8 @@
 package com.babysleep
 
-import android.content.Intent
+import android.content.Context
+import android.net.nsd.NsdManager
+import android.net.nsd.NsdServiceInfo
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -17,23 +19,97 @@ import androidx.camera.video.VideoCapture
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import com.google.common.util.concurrent.ListenableFuture
+import java.net.ServerSocket
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class ChildActivity : AppCompatActivity() {
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
     private lateinit var cameraExecutor: ExecutorService
+    private var mServiceName = ""
+    private var nsdManager : NsdManager? = null
+    private var serverSocket : ServerSocket? = null
+    private var registrationListener : NsdManager.RegistrationListener? = null
+
+
+    var mLocalPort: Int = 0
+
+    override fun onDestroy() {
+        nsdManager?.unregisterService(registrationListener)
+
+        //connection.tearDown()
+        super.onDestroy()
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_child)
 
         findViewById<Button>(R.id.start_record).setOnClickListener {
+            // Start and stop discovery
+            enableDiscovery()
             // Start and stop recording
         }
 
         initializeCamera()
 
+    }
+
+    private fun disableDiscovery() {
+
+    }
+
+    private fun enableDiscovery() {
+        initializeServerSocket()
+        registerService(mLocalPort)
+    }
+
+    private fun registerService(port: Int) {
+        // Create the NsdServiceInfo object, and populate it.
+        val serviceInfo = NsdServiceInfo().apply {
+            // The name is subject to change based on conflicts
+            // with other services advertised on the same network.
+            serviceName = "babysleep"
+            serviceType = "_babysleep._udp"
+            setPort(port)
+        }
+
+        nsdManager = (getSystemService(Context.NSD_SERVICE) as NsdManager).apply {
+            registrationListener = object : NsdManager.RegistrationListener {
+
+                override fun onServiceRegistered(nsdServiceInfo: NsdServiceInfo) {
+                    // Save the service name. Android may have changed it in order to
+                    // resolve a conflict, so update the name you initially requested
+                    // with the name Android actually used.
+                    mServiceName = nsdServiceInfo.serviceName
+                }
+
+                override fun onRegistrationFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
+                    // Registration failed! Put debugging code here to determine why.
+                }
+
+                override fun onServiceUnregistered(arg0: NsdServiceInfo) {
+                    // Service has been unregistered. This only happens when you call
+                    // NsdManager.unregisterService() and pass in this listener.
+                }
+
+                override fun onUnregistrationFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
+                    // Unregistration failed. Put debugging code here to determine why.
+                }}
+
+            registerService(serviceInfo, NsdManager.PROTOCOL_DNS_SD, registrationListener)
+        }
+
+    }
+
+    private fun initializeServerSocket() {
+        // Initialize a server socket on the next available port.
+        serverSocket = ServerSocket(0).also { socket ->
+            // Store the chosen port.
+            mLocalPort = socket.localPort
+            registerService(mLocalPort)
+        }
     }
 
     private fun initializeCamera() {
--- app/src/main/java/com/babysleep/ParentActivity.kt
+++ app/src/main/java/com/babysleep/ParentActivity.kt
@@ -1,11 +1,17 @@
 package com.babysleep
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ParentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parent)
+
+
+        findViewById<Button>(R.id.connect).setOnClickListener {
+
+        }
     }
 }
